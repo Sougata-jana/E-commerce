001369bae4813b3706224f0f5e95c24182836ba1
Sougata Jana <sougataj76@gmail.com>
2025-08-26 19:34:41 +0530

    Add routing and create initial page components
    - Implement routing in App component using react-router-dom
    - Create About, Cart, Collection, Home, Login, Order, PlaceOrder, and Product components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div >
-      App
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]' >
+      <Routes>
+        
+      </Routes>
 
     </div>
   )
