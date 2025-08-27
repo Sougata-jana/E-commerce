327dd5d78a041ecce98647baa3af4d58503b7e9b
Sougata Jana <sougataj76@gmail.com>
2025-08-27 15:29:52 +0530

    Add routing for main application pages and create Contact component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
+import Home from "./page/Home"
+import About from './page/About'
+import Contact from './page/Contact'
+import Collection from './page/Collection'
+import Login from './page/Login'
+import Order from './page/Order'
+import Cart from './page/Cart'
+import PlaceOrder from './page/PlaceOrder'
 
 function App() {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]' >
       <Routes>
-        
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/order' element={<Order/>}/>
+        <Route path='/product/:productID' element={<Product/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/placeorder' element={<PlaceOrder/>}/>
       </Routes>
 
     </div>
