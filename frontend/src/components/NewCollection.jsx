58fd7a4aa2db25668929db21d16f0c9b3d48e424
Sougata Jana <sougataj76@gmail.com>
2025-09-05 10:57:25 +0530

    Add ProductIteams component and implement latest products display in NewCollection

--- frontend/src/components/NewCollection.jsx
+++ frontend/src/components/NewCollection.jsx
@@ -1,16 +1,29 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { shopContext } from '../context/ShopContext'
 import Title from './Title'
+import ProductIteams from './ProductIteams'
 
 function NewCollection() {
     const {products} = useContext(shopContext)
+    const [latestProducts, setLeatestProducts] = useState([])
 
+    useEffect(()=>{
+      setLeatestProducts(products.slice(0,10))
+    },[])
     
   return (
     <div className='my-10'>
         <div className='text-center py-8 text-3xl'>
             <Title text1={"LATEST"} text2={" COLLECTION"}/>
 
+        </div>
+                {/* Rendaring products */}
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+          {
+            latestProducts.map((items,index)=>(
+              <ProductIteams key={index} image={items.image} name={items.name} price={items.price}/>
+            ))
+          }
         </div>
     </div>
   )
